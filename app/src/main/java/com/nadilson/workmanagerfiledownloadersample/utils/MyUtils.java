26bde7a946ded6e70abf27585460081af753c09d
Nadilson <nadilson@protonmail.com>
2025-01-17 18:01:45 -0300

    feat: Utils

--- app/src/main/java/com/nadilson/workmanagerfiledownloadersample/utils/MyUtils.java
+++ app/src/main/java/com/nadilson/workmanagerfiledownloadersample/utils/MyUtils.java
@@ -1,11 +1,22 @@
 package com.nadilson.workmanagerfiledownloadersample.utils;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
+
+import androidx.core.app.ActivityCompat;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MyUtils {
+
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+
+    private static final String[] PERMISSIONS_STORAGE = {
+            android.Manifest.permission.READ_EXTERNAL_STORAGE,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
 
     public static String getFormattedFileSize(Context context, long fileSize) {
         return android.text.format.Formatter.formatFileSize(context, fileSize);
@@ -18,5 +29,16 @@ public class MyUtils {
             return matcher.group(1);
         }
         return null;
+    }
+
+    public static void verifyStoragePermissions(Activity activity) {
+        int permission = ActivityCompat.checkSelfPermission(activity, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
     }
 }
