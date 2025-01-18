b2aad9942de1f2b3ec608b79d66c98e9f0f3a197
Nadilson <nadilson@protonmail.com>
2025-01-18 16:17:16 -0300

    feat: FileDownloadWorker final code and MainActivity updated code

--- app/src/main/java/com/nadilson/workmanagerfiledownloadersample/MainActivity.java
+++ app/src/main/java/com/nadilson/workmanagerfiledownloadersample/MainActivity.java
@@ -2,19 +2,29 @@ package com.nadilson.workmanagerfiledownloadersample;
 
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
-
+import androidx.lifecycle.Observer;
 import androidx.navigation.ui.AppBarConfiguration;
+import androidx.work.Constraints;
+import androidx.work.Data;
+import androidx.work.NetworkType;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkInfo;
+import androidx.work.WorkManager;
 
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+import com.nadilson.workmanagerfiledownloadersample.constants.MyConstants;
 import com.nadilson.workmanagerfiledownloadersample.databinding.ActivityMainBinding;
 import com.nadilson.workmanagerfiledownloadersample.utils.MyUtils;
+import com.nadilson.workmanagerfiledownloadersample.workmanager.FileDownloaderWorker;
 
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -25,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ProgressBar progressBar;
     private TextView progressText;
+    private TextInputEditText textInputEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        MyUtils.verifyStoragePermissions(MainActivity.this);
+
         setSupportActionBar(binding.toolbar);
 
         progressBar = findViewById(R.id.progressBar);
@@ -40,7 +53,71 @@ public class MainActivity extends AppCompatActivity {
         progressText.setText(String.format(getString(R.string.file_download_percentage),
                 MyUtils.getFormattedFileSize(this, 0),
                 MyUtils.getFormattedFileSize(this, 0),
-                String.valueOf(progressBar.getProgress())));
+                "0"));
+        Button button = findViewById(R.id.download_button);
+        textInputEditText = findViewById(R.id.textInputEditText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!TextUtils.isEmpty(textInputEditText.getText())) {
+                    Data inputData = new Data.Builder()
+                            .putString(MyConstants.FILE_URL_DATA_KEY, String.valueOf(textInputEditText.getText()))
+                            .putString(MyConstants.FILE_PATH_DATA_KEY, MyConstants.APP_DOWNLOAD_PATH +
+                                    MyUtils.extractFilename(String.valueOf(textInputEditText.getText())))
+                            .build();
+
+                    OneTimeWorkRequest fileDownloadOneTimeWorkRequest = new OneTimeWorkRequest.Builder(FileDownloaderWorker.class)
+                            .setInputData(inputData)
+                            .setConstraints(new Constraints.Builder()
+                                    .setRequiredNetworkType(NetworkType.CONNECTED)
+                                    .setRequiresStorageNotLow(true)
+                                    .build())
+                            .build();
+
+                    WorkManager.getInstance(MainActivity.this).enqueue(fileDownloadOneTimeWorkRequest);
+
+                    WorkManager.getInstance(MainActivity.this)
+                            .getWorkInfoByIdLiveData(fileDownloadOneTimeWorkRequest.getId())
+                            .observe(MainActivity.this, new Observer<WorkInfo>() {
+                                @Override
+                                public void onChanged(WorkInfo workInfo) {
+                                    if (workInfo != null) {
+                                        try {
+                                            if (workInfo.getState() == WorkInfo.State.SUCCEEDED) {
+                                                Snackbar.make(view, "Download finalizado com sucesso!", Snackbar.LENGTH_LONG)
+                                                        .setAnchorView(R.id.progressBarText)
+                                                        .setAction("Action", null).show();
+                                            } else if (workInfo.getState() == WorkInfo.State.FAILED) {
+                                                Snackbar.make(view, "Falha ao realizar o download do arquivo!", Snackbar.LENGTH_LONG)
+                                                        .setAnchorView(R.id.progressBarText)
+                                                        .setAction("Action", null).show();
+                                            }
+                                        } catch (Exception exception) {
+                                            Snackbar.make(view, "Erro: " + exception.getLocalizedMessage(), Snackbar.LENGTH_SHORT)
+                                                    .setAnchorView(R.id.progressBarText)
+                                                    .setAction("Action", null).show();
+                                        }
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+
+        FileDownloaderWorker.progressLiveData.observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer progress) {
+                progressBar.setProgress(progress);
+            }
+        });
+
+        FileDownloaderWorker.progressTextLiveData.observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String progressInfo) {
+                progressText.setText(progressInfo);
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/nadilson/workmanagerfiledownloadersample/workmanager/FileDownloaderWorker.java
+++ app/src/main/java/com/nadilson/workmanagerfiledownloadersample/workmanager/FileDownloaderWorker.java
@@ -1,19 +1,99 @@
 package com.nadilson.workmanagerfiledownloadersample.workmanager;
 
 import android.content.Context;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.lifecycle.MutableLiveData;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
+import com.nadilson.workmanagerfiledownloadersample.R;
+import com.nadilson.workmanagerfiledownloadersample.constants.MyConstants;
+import com.nadilson.workmanagerfiledownloadersample.utils.MyUtils;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class FileDownloaderWorker extends Worker {
+
+    Context context;
+
+    public static MutableLiveData<Integer> progressLiveData = new MutableLiveData<>();
+    public static MutableLiveData<String> progressTextLiveData = new MutableLiveData<>();
+
     public FileDownloaderWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
+
+        this.context = context;
     }
 
     @NonNull
     @Override
     public Result doWork() {
-        return null;
+        String fileUrl = getInputData().getString(MyConstants.FILE_URL_DATA_KEY);
+        String filePath = getInputData().getString(MyConstants.FILE_PATH_DATA_KEY);
+
+        if (fileUrl == null) {
+            Log.e(MyConstants.APP_TAG, "O Worker não recebeu o URL!");
+            return Result.failure();
+        }
+
+        if (filePath == null) {
+            Log.e(MyConstants.APP_TAG, "O Worker não recebeu o caminho do arquivo!");
+            return Result.failure();
+        }
+
+        try {
+            URL url = new URL(fileUrl);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            //   httpURLConnection.setRequestMethod("GET");
+            httpURLConnection.connect();
+
+            if (httpURLConnection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                Log.e(MyConstants.APP_TAG, "Erro! Código " +
+                        httpURLConnection.getResponseCode() + ": " + httpURLConnection.getResponseMessage());
+                return Result.failure();
+            }
+
+            long fileSize = httpURLConnection.getContentLengthLong();
+
+
+            InputStream inputStream = new BufferedInputStream(httpURLConnection.getInputStream());
+            FileOutputStream fileOutputStream = new FileOutputStream(filePath);
+
+            byte[] data = new byte[1024];
+            int count = 0;
+            long totalWritten = 0L;
+
+            while ((count = inputStream.read(data)) > 0) {
+                fileOutputStream.write(data, 0, count);
+                totalWritten += count;
+
+                int progress = (int) ((totalWritten * 100L) / fileSize);
+
+                progressLiveData.postValue(progress);
+                progressTextLiveData.postValue(String.format(context.getResources().getString(R.string.file_download_percentage),
+                        MyUtils.getFormattedFileSize(context, (int) totalWritten),
+                        MyUtils.getFormattedFileSize(context, fileSize),
+                        String.valueOf(progress)));
+            }
+
+            fileOutputStream.flush();
+            fileOutputStream.close();
+            inputStream.close();
+            httpURLConnection.disconnect();
+
+            Log.d(MyConstants.WORKER_TAG, "Download finalizado: " + filePath);
+
+            return Result.success();
+
+        } catch (Exception ex) {
+            Log.e(MyConstants.WORKER_TAG, "Erro ao realizar o download do arquivo! Descrição do erro: ", ex);
+            return Result.failure();
+        }
     }
 }
